2e57227d9e2cc347df119a6372c200b78655ac3e
Prasad Lanka <86981186+Prasadl97@users.noreply.github.com>
2026-03-12 14:36:24 +0530

    Update admin-login.steps.ts

--- src/step-definitions/admin-login.steps.ts
+++ src/step-definitions/admin-login.steps.ts
@@ -1,11 +1,14 @@
 import { Given, When, Then } from '@cucumber/cucumber';
 import { PlaywrightWorld } from '../support/world.js';
+import { loadTestData } from '../../configs/testData.js';
 
 Given('I am on the admin login page', async function (this: PlaywrightWorld) {
   await this.adminLoginPage.gotoAdminLogin();
 });
 
-When('I log in to the admin with username {string} and password {string}', async function (this: PlaywrightWorld, username: string, password: string) {
+When('I log in as the admin', async function (this: PlaywrightWorld) {
+  const testData = await loadTestData();
+  const { username, password } = testData.admin;
   await this.adminLoginPage.login(username, password);
 });
 
